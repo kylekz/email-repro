684e7c92e7cc58564130d717ffe2a59efa4c7ee2
kylekz <kairu@team-reflex.com>
2024-08-04 23:59:30 +1200

    ?

--- apps/web/src/app/actions.ts
+++ apps/web/src/app/actions.ts
@@ -1,12 +1,24 @@
 "use server";
 
+// import { render } from "@react-email/render";
 import { RemovedFromOrganization } from "@repo/email/emails/removed-from-org";
+import { Resend } from "resend";
+
+// uncomment this to get it working
+// const resend = new Resend("");
 
 export async function email() {
-  const rendered = RemovedFromOrganization({
+  const react = RemovedFromOrganization({
     baseUrl: "",
     receiverName: "",
     organizationName: "",
   });
-  console.log(rendered);
+
+  // const result = await resend.emails.send({
+  //   from: "example@example.com",
+  //   to: "example@example.com",
+  //   subject: "Removed from Organization",
+  //   react: react,
+  // });
+  // console.log(result);
 }
